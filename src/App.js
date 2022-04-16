4f013d49911507e374e2942771cb3e90949f29e5
mdsaharshital <mdsaharshitol@gmail.com>
2022-04-17 01:24:54 +0600

    footer section done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home/Home/Home";
 import Footer from "./Pages/shared/Footer/Footer";
@@ -5,7 +6,10 @@ import Footer from "./Pages/shared/Footer/Footer";
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+
       <Footer />
     </div>
   );
--- src/Pages/shared/Footer/Footer.js
+++ src/Pages/shared/Footer/Footer.js
@@ -1,11 +1,31 @@
 import React from "react";
 import "./Footer.css";
+import { BsFacebook, BsInstagram, BsLinkedin } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <div>
-      <h2>Footer is awesome</h2>
-    </div>
+    <footer className="d-flex flex-column justify-content-center align-items-center">
+      <h3 className="footer-title ">Md Shahar Shitol</h3>
+      <p className="mb-5">
+        <small className="pro ">Production</small>
+      </p>
+      <div className="social-icon mb-4">
+        <a href="http://" target="_blank" rel="noopener noreferrer">
+          <BsFacebook />
+        </a>
+        <a href="http://" target="_blank" rel="noopener noreferrer">
+          <BsInstagram />
+        </a>
+        <a href="http://" target="_blank" rel="noopener noreferrer">
+          <BsLinkedin />
+        </a>
+      </div>
+      <p className="mt-5">
+        <small className="copyright-text">
+          Copyright © by Md shahar Shital
+        </small>
+      </p>
+    </footer>
   );
 };
 
