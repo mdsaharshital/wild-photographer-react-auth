f2c3ad4db1675781baa6a8a86207c2f724358af6
mdsaharshital <mdsaharshitol@gmail.com>
2022-04-18 03:58:36 +0600

    netlify reload issue

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -3,7 +3,42 @@ import React from "react";
 const Blogs = () => {
   return (
     <div>
-      <h1 className="text-center mt-5 mb-2">My Blogs</h1>
+      <h1 className="text-center mt-5 mb-2 ">My Blogs</h1>
+      <div className="row g-2 m-2">
+        <div className="border p-3 col-12 col-md-6 col-lg-6 ">
+          <h3>
+            What other services does firebase provide other than authentication?
+          </h3>
+          <p>
+            <span className="fw-bolder">Answer:</span> FireBase provides many
+            other services which are given below:
+          </p>
+          <ul>
+            <li>Realtime database</li>
+            <li>Remote Config</li>
+            <li>Firebase Ml</li>
+            <li>Cloud functions</li>
+            <li>Authenticaion</li>
+            <li>Cloud Messaging</li>
+            <li>Hosting</li>
+            <li>Cloud Storage</li>
+          </ul>
+        </div>
+        <div className="border p-3 col-12 col-md-6 col-lg-6">
+          <h3>Difference between authorization and authentication?</h3>
+          <p>
+            <span className="fw-bolder">Answer:</span> Authentication is the act
+            of confirming a person's identity, whereas authorisation is the
+            process of confirming a user's access to certain apps, files, and
+            data. Authentication and authorisation serve the same purpose in the
+            digital age. Authentication is a technique for ensuring that people
+            are who they say they are. After this has been verified,
+            authorisation is used to provide the user authority to access
+            various levels of information and execute certain duties, based on
+            the rules defined for various sorts of users.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
